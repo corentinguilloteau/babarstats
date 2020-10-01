6b129d85378355f4e8704f04bbe7b10ff0978a48
Guilloteau Corentin <corentin.guilloteau@telecom-paris.fr>
2020-10-01 18:02:55 +0200

    trust proxy

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', 1)
 
 app.use(logger('dev'));
 app.use(express.json());
